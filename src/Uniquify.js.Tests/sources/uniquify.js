80cce4432851f217856e5e022d63d3ff8499f816
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-01-22 13:58:35 +0000

    update uniquify.js to accept special characters

--- src/Uniquify.js.Tests/sources/uniquify.js
+++ src/Uniquify.js.Tests/sources/uniquify.js
@@ -1,23 +1,23 @@
 ﻿(function () {
     this.Uniquify = Class.extend({
-        generatePassword: function(domain, key, length, includeSpecial, iterations) {
+        generatePassword: function(domain, key, length, includeSpecial, iterations, specialChars) {
             if (!iterations) iterations = 1;
+            if (!specialChars) specialChars = ['!', '£', '$', '%', '&', '*', '@', '~', '#', '.', '<', '>', '?', ';', ':', '_', '+'];
             var password = key;
             for (var i = 0; i < iterations; i++) {
-                password = this.getHash(domain, password, length, includeSpecial);
+                password = this.getHash(domain, password, length, includeSpecial, specialChars);
             }
             return password;
         },
-        getHash: function(domain, key, length, includeSpecial) {
+        getHash: function (domain, key, length, includeSpecial, specialChars) {
             var hash = hex_hmac_sha256(key, domain);
-            return this.applySettings(hash, length, includeSpecial);
+            return this.applySettings(hash, length, includeSpecial, specialChars);
         },
-        applySettings: function (hash, length, includeSpecial) {
+        applySettings: function (hash, length, includeSpecial, specialChars) {
             var chars = [];
-            var defaultSpecials = ['!', '£', '$', '%', '&', '*', '@', '~', '#', '.', '<', '>', '?', ';', ':', '_', '+'];
             var standardChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
             if (includeSpecial) {
-                chars = chars.concat(defaultSpecials);
+                chars = chars.concat(specialChars);
             }
             chars = chars.concat(standardChars);
             var bytes = [];
@@ -37,13 +37,13 @@
 
             if (includeSpecial) {
                 var found = false;
-                for (var s = 0; s < defaultSpecials.length; ++s) {
-                    if (securepass.indexOf(defaultSpecials[s]) !== -1) {
+                for (var s = 0; s < specialChars.length; ++s) {
+                    if (securepass.indexOf(specialChars[s]) !== -1) {
                         found = true;
                     }
                 }
                 if (!found) {
-                    securepass = securepass.replaceAt(bytes[length % bytes.length] % length, defaultSpecials[hash.length % defaultSpecials.length]);
+                    securepass = securepass.replaceAt(bytes[length % bytes.length] % length, specialChars[hash.length % specialChars.length]);
                 }
             }
 
