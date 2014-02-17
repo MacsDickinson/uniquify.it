a6c484fe4d246f44141cbeecb2dd6b4cbbd67181
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-02-17 17:08:54 +0000

    wired up form on popup

--- Chrome Extension/scripts/contentscript.js
+++ Chrome Extension/scripts/contentscript.js
@@ -27,46 +27,56 @@ if ($("input:password").length) {
 				left: pwWidth - paddingRight - 12
 			});
 			$('body').append('<div id="uniquify-popup'+index+'" class="uniquify-popup" >'+
-				'<span class="uniquify-h3">Generate Your Secure Password:</span>' +
-				'<div class="uniquify-q">' +
-					'<label for="uniquify-domain">Domain Name:</label>' +
-					'<input id="uniquify-domain" name="uniquify-domain" placeholder="eg. google.com" type="text" value="'+ document.domain +'">' +
+				'<div class="uniquify-form">' +
+					'<span class="uniquify-h3">Generate Your Secure Password:</span>' +
+					'<div class="uniquify-q">' +
+						'<label for="uniquify-domain">Domain Name:</label>' +
+						'<input id="uniquify-domain" class="uniquify-domain" name="uniquify-domain" placeholder="eg. google.com" type="text" value="'+ document.domain +'">' +
+					'</div>' +
+					'<div class="uniquify-q">' +
+						'<label for="uniquify-key0">Key:</label>' +
+						'<input id="uniquify-key0" name="uniquify-key0" class="uniquify-key no-uniquify-overlay" type="password" value="" />' +
+						'<a class="uniquify-show-key"><i class="uniquify-icon-eye"></i></a>' +
+					'</div>' +
+					'<span for="key0" class="uniquify-error" style="display: none;"></span>' +
+					'<div class="uniquify-q">' +
+						'<label for="uniquify-add-key"></label>' +
+						'<a class="uniquify-add-key">Add Key<i class="uniquify-icon-plus2"></i></a>' +
+					'</div>' +
+					'<div class="uniquify-q">' +
+						'<label for="uniquify-length">Password Length:</label>' +
+						'<div id="uniquify-length-slider" class="uniquify-slider"></div>' +
+						'<span id="uniquify-length" class="uniquify-slider-label"></span>' +
+						'<input type="hidden" id="uniquify-length-value"/>' +
+					'</div>' +
+					'<div class="uniquify-q">' +
+						'<label for="uniquify-special">Special Characters:</label>' +
+						'<input id="uniquify-special" name="uniquify-special" type="checkbox" checked="checked">' +
+					'</div>' +
+					'<div class="uniquify-q" id="uniquify-special-container">' +
+						'<label for="uniquify-special-chars"></label>' +
+						'<input id="uniquify-special-chars" name="uniquify-special-chars" type="text" value="!£$%&*@~#.<>?;:_+">' +
+					'</div>' +
+					'<div class="uniquify-q">' +
+						'<label for="uniquify-iterations">Iterations:</label>' +
+						'<div id="uniquify-iterations-slider" class="uniquify-slider"></div>' +
+						'<span id="uniquify-iterations" class="uniquify-slider-label"></span>' +
+						'<input type="hidden" id="uniquify-iterations-value" />' +
+					'</div>' +
+					'<span for="uniquify-iterations" class="uniquify-warning" id="uniquify-iterations-error">We recommend you use a higher number of iterations</span>' +
+					'<div class="uniquify-actons">' +
+						'<input type="submit" class="uniquify-it" value="Generate Password" />' +
+					'</div>' +
 				'</div>' +
-				'<div class="uniquify-q">' +
-					'<label for="uniquify-key0">Key:</label>' +
-					'<input id="uniquify-key0" name="uniquify-key0" class="uniquify-key no-uniquify-overlay" type="password" value="" />' +
-					'<a class="uniquify-show-key"><i class="uniquify-icon-eye"></i></a>' +
-				'</div>' +
-				'<span for="key0" class="uniquify-error" style="display: none;"></span>' +
-				'<div class="uniquify-q">' +
-					'<label for="uniquify-add-key"></label>' +
-					'<a class="uniquify-add-key">Add Key<i class="uniquify-icon-plus2"></i></a>' +
-				'</div>' +
-				'<div class="uniquify-q">' +
-					'<label for="uniquify-length">Password Length:</label>' +
-					'<div id="uniquify-length-slider" class="uniquify-slider"></div>' +
-					'<span id="uniquify-length" class="uniquify-slider-label"></span>' +
-					'<input type="hidden" id="uniquify-length-value"/>' +
-				'</div>' +
-				'<div class="uniquify-q">' +
-					'<label for="uniquify-special">Special Characters:</label>' +
-					'<input id="uniquify-special" name="uniquify-special" type="checkbox" checked="checked">' +
-				'</div>' +
-				'<div class="uniquify-q" id="uniquify-special-container">' +
-					'<label for="uniquify-special-chars"></label>' +
-					'<input id="uniquify-special-chars" name="uniquify-special-chars" type="text" value="!£$%&*@~#.<>?;:_+">' +
-				'</div>' +
-				'<div class="uniquify-q">' +
-					'<label for="uniquify-iterations">Iterations:</label>' +
-					'<div id="uniquify-iterations-slider" class="uniquify-slider"></div>' +
-					'<span id="uniquify-iterations" class="uniquify-slider-label"></span>' +
-					'<input type="hidden" id="uniquify-iterations-value" />' +
-				'</div>' +
-				'<span for="uniquify-iterations" class="uniquify-warning" id="uniquify-iterations-error">We recommend you use a higher number of iterations</span>' +
-				'<div class="uniquify-actons">' +
-					'<input type="submit" class="uniquify-it" for="' + id + '" value="Generate Password" />' +
-				'</div>' +
-			'</div>');
+			    '<div class="uniquify-answer-container">' +
+			        '<h1>' +
+			            '<span id="uniquify-result" class="uniquify-result"></span>' +
+			        '</h2>' +
+			        '<div class="uniquify-actons">' +
+			            '<input type="submit" id="uniquify-insert" class="uniquify-insert" for="' + id + '" value="Insert" />' +
+			        '</div>' +
+		        '</div>' +
+		    '</div>');
 		}
 	});
 	
@@ -89,14 +99,6 @@ $('.uniquify-btn').click(function(event) {
 	clickedPassID = $('.uniquify-click', $(this).parents('.uniquify-wrapper:first')).attr('name');
 	$('.uniquify-click', $(this).parents('.uniquify-wrapper:first')).click();
 });
-var uniquify = new Uniquify();
-$('.uniquify-it').click(function () {
-	var input = $(this).attr('for');
-	var domain = $('.uniquify-domain', $(this).parents('.uniquify-popup:first')).val();
-	var key = $('.uniquify-key', $(this).parents('.uniquify-popup:first')).val();
-	var length = $('.uniquify-length', $(this).parents('.uniquify-popup:first')).val();
-	submitPass(domain, key, length, input);
-});
 $("#uniquify-length-slider").noUiSlider({
     range: [8, 24],
     start: 8,
@@ -111,14 +113,13 @@ $("#uniquify-length-slider").noUiSlider({
         $('#uniquify-length-value').val(length);
     }
 });
-$(".uniquify-iterations-slider").noUiSlider({
+$("#uniquify-iterations-slider").noUiSlider({
     range: [1, 10000],
     start: 1000,
     step: 1,
     handles: 1,
     serialization: {
-        resolution: 1,
-        to: [$("#uniquify-iterations-value"), $('#uniquify-iterations')]
+        resolution: 1
     },
     slide: function() {
         var iterations = $("#uniquify-iterations-slider").val();
@@ -145,22 +146,44 @@ $('.uniquify-add-key').click(function () {
 				'<span for="key' + id + '" class="uniquify-error" style="display: none;"></span>'
     $(this).parent().before(html);
 });
-var submitPass = function (domain, key, length, input) {
-	var result = generatePass(domain, key, length);
+var uniquify = new Uniquify();
+$('.uniquify-it').click(function () {
+	var domain = $('.uniquify-domain', $(this).parents('.uniquify-popup:first')).val();
+	var keys = [];
+    $('.uniquify-key', $(this).parents('.uniquify-popup:first')).each(function () {
+        var val = $(this).val();
+        if (val !== '') {
+            keys.push($(this).val());
+        }
+    });
+	var length = parseInt($('#uniquify-length-slider', $(this).parents('.uniquify-popup:first')).val());
+    var special = $('#uniquify-special')[0].checked;
+    var specials = $('#uniquify-special-chars').val();
+    var iterations = parseInt($('#uniquify-iterations-slider', $(this).parents('.uniquify-popup:first')).val());
+	submitPass(domain, keys, length, special, iterations, specials);
+});
+var submitPass = function (domain, keys, length, includeSpecial, iterations, specialChars) {
+	var result = generatePass(domain, keys, length, includeSpecial, iterations, specialChars);
 	if (result) {
-		$('#'+input).val(result);
-		$('#'+input).focus();
-		$("#lean_overlay").click();
+		$('.uniquify-result').text(result);
+		$('.uniquify-answer-container').show();
 	}
 }
-var generatePass = function (domain, key, length) {
+var generatePass = function (domain, keys, length, includeSpecial, iterations, specialChars) {
 	$('.uniquify-error').hide();
-	if (key.length > 7) {
-		var result = uniquify.generatePassword(domain, key, length, true, 1);
-		return result;
+	var result = uniquify.generatePassword(domain, keys, length, includeSpecial, iterations, specialChars);
+	return result;
+	/*if (key.length > 7) {
 	} else {
 		$('.uniquify-error').text('Secret word needs to be at least 8 characters');
 		$('.uniquify-error').show();
 		return;
-	}
+	}*/
 };
+$('.uniquify-insert').click(function() {
+	var input = $(this).attr('for');
+	var result = $('.uniquify-result', $(this).parents('.uniquify-popup:first')).text();
+	$('#'+input).val(result);
+	$('#'+input).focus();
+	$("#lean_overlay").click();
+});
